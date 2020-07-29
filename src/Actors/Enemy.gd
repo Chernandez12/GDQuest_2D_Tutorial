79e7dd85d53cef0cb892bf23f95cbcef9d8855b1
Carlos_Hernandez <carlosandrehernandez@hotmail.com>
2020-07-29 12:21:52 -0700

    Added Enemy Health

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -1,5 +1,7 @@
 extends "res://src/Actors/Actor.gd"
 
+export var health = 3
+
 func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -speed.x
@@ -7,8 +9,11 @@ func _ready() -> void:
 func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
 	if body.global_position.y > get_node("StompDetector").global_position.y:
 		return
-	get_node("CollisionShape2D").disabled = true
-	queue_free()
+	health -= 1
+	print(health)
+	if health == 0:
+		get_node("CollisionShape2D").disabled = true
+		queue_free()
 
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
