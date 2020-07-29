5390f317a056a4852c3c00c6dfd39cb5a6916e74
Carlos_Hernandez <carlosandrehernandez@hotmail.com>
2020-07-29 11:30:48 -0700

    Global Coin Counter

--- src/Objects/Coin.gd
+++ src/Objects/Coin.gd
@@ -3,4 +3,7 @@ extends Area2D
 onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 
 func _on_Coin_body_entered(body: PhysicsBody2D) -> void:
+	global.coins += 1
+	
+	print("Coins = ", global.coins)
 	anim_player.play("fade_out")
---
+++ src/global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var coins = 0
+var lives = 5
