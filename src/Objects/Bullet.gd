f21f461ea180fef9efaf76d605c5fd5ad1ec0b02
Conner Hinkle <connerhinkle@gmail.com>
2020-08-08 11:40:43 -0700

    Ranged projectiles now arc

--- src/Objects/Bullet.gd
+++ src/Objects/Bullet.gd
@@ -1,21 +1,23 @@
 extends RigidBody2D
 
 export var projectile_speed = 500
-export var lifetime = 0.5
-var velocity = Vector2()
+export var lifetime = 2
+var velocity = Vector2.ZERO
 var direction = 1
 
 func _physics_process(delta):
-	global_position.x += projectile_speed * delta * direction
+	velocity.y += global.gravity * delta
+	get_node(".").set_linear_velocity(velocity)
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if global.facing_right:
 		direction = 1
-		get_node("Particles2D").rotation_degrees = 180
+		rotation = deg2rad(180)
 	else:
 		direction = -1
-		get_node("Particles2D").rotation_degrees = -180
+		rotation = deg2rad(0)
+	velocity = Vector2(projectile_speed * direction, -400)
 	set_physics_process(true)
 	SelfDestruct()
 	
--- src/global.gd
+++ src/global.gd
@@ -3,3 +3,4 @@ extends Node
 var coins = 0
 var lives = 5
 var facing_right = true
+var gravity = 1000
