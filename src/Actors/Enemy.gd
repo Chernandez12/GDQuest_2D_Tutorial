fd8465bb779eb3f339ce169567f67a8af5358b3b
Conner Hinkle <connerhinkle@gmail.com>
2020-08-08 20:31:14 -0700

    Added enemy knockback

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -2,15 +2,31 @@ extends "res://src/Actors/Actor.gd"
 
 export var health = 3
 onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+onready var player = global.get("player")
+
+var stunned = false
+var stunTime = 0.4
+var knockBackTimer = Timer.new()
+var knockBackVector = Vector2(600, -600)
 
 func _ready() -> void:
 	set_physics_process(true)
 	_velocity.x = -speed.x
+	knockBackTimer.set_wait_time(stunTime)
+	knockBackTimer.connect("timeout", self, "_on_knockBackTimer_timeout") 
+	knockBackTimer.set_one_shot(true)
+	add_child(knockBackTimer)
 
 func _on_StompDetector_area_entered(area: Area2D) -> void:
 	health -= 1
 	anim_player.play("damage_taken")
 	print(health)
+	
+	_velocity = knockBackVector
+	if self.global_transform.origin.x < player.global_transform.origin.x:
+		_velocity.x *= -1
+	knockBackTimer.start()
+	
 	#get_node("CollisionShape2D").disabled = true
 	#queue_free()
 	if health == 0:
@@ -36,4 +52,7 @@ func _physics_process(delta: float) -> void:
 	if is_on_wall():
 		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
-
+	
+func _on_knockBackTimer_timeout():
+	stunned = false
+	_velocity.x = -speed.x
