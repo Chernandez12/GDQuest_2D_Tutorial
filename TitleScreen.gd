088c6bce7d655a769968b976839cc7549f456a56
Nanonaitor <vnanoc@hotmail.com>
2020-08-09 00:54:19 -0700

    Basic Start/Pause
    - Made Start Menu
    - Made Pause Menu
    - Pause accesible via "esc" key
    - Added start/quit functionality to start menu
    - Added full functionality to Start Menu

---
+++ TitleScreen.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+
+func _ready():
+	$MarginContainer/VBoxContainer/VBoxContainer/StartButton.grab_focus()
+
+
+func _phyisics_process(delta):
+	if $MarginContainer/VBoxContainer/VBoxContainer/StartButton.is_hovered():
+		$MarginContainer/VBoxContainer/VBoxContainer/StartButton.grab_focus()
+	if $MarginContainer/VBoxContainer/VBoxContainer/CreditsButton.is_hovered():
+		$MarginContainer/VBoxContainer/VBoxContainer/CreditsButton.grab_focus()
+	if $MarginContainer/VBoxContainer/VBoxContainer/ControlsButton.is_hovered():
+		$MarginContainer/VBoxContainer/VBoxContainer/ControlsButton.grab_focus()
+	if $MarginContainer/VBoxContainer/VBoxContainer/QuitButton.is_hovered():
+		$MarginContainer/VBoxContainer/VBoxContainer/QuitButton.grab_focus()
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://Levels/LevelTemplate.tscn")
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
