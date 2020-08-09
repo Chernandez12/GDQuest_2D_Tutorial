9fe62d5191bef8e34fc4b736789c202b365af065
Nanonaitor <vnanoc@hotmail.com>
2020-08-09 01:30:00 -0700

    Minor Tweeks
    Adjusted Main menu so it uses normal buttons instead of texture buttons

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -9,16 +9,15 @@ func _ready():
 func _phyisics_process(delta):
 	if $MarginContainer/VBoxContainer/VBoxContainer/StartButton.is_hovered():
 		$MarginContainer/VBoxContainer/VBoxContainer/StartButton.grab_focus()
-	if $MarginContainer/VBoxContainer/VBoxContainer/CreditsButton.is_hovered():
-		$MarginContainer/VBoxContainer/VBoxContainer/CreditsButton.grab_focus()
-	if $MarginContainer/VBoxContainer/VBoxContainer/ControlsButton.is_hovered():
-		$MarginContainer/VBoxContainer/VBoxContainer/ControlsButton.grab_focus()
+#	if $MarginContainer/VBoxContainer/VBoxContainer/CreditsButton.is_hovered():
+#		$MarginContainer/VBoxContainer/VBoxContainer/CreditsButton.grab_focus()
+#	if $MarginContainer/VBoxContainer/VBoxContainer/ControlsButton.is_hovered():
+#		$MarginContainer/VBoxContainer/VBoxContainer/ControlsButton.grab_focus()
 	if $MarginContainer/VBoxContainer/VBoxContainer/QuitButton.is_hovered():
 		$MarginContainer/VBoxContainer/VBoxContainer/QuitButton.grab_focus()
 
-
 func _on_StartButton_pressed():
-	get_tree().change_scene("res://Levels/LevelTemplate.tscn")
+	get_tree().change_scene("res://Scenes/LevelTemplate.tscn")
 
 
 func _on_QuitButton_pressed():
