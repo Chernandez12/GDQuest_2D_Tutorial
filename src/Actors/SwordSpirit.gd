d3a7d9a908cb63f785a343b49c86686cec44ca8f
Suttungr <steinbrenner.van@gmail.com>
2020-08-08 23:35:42 -0700

    Added Enemy.gd script to Sword Spirit

--- src/Actors/SwordSpirit.gd
+++ src/Actors/SwordSpirit.gd
@@ -1,16 +1,16 @@
 extends "res://src/Actors/Actor.gd"
 
 
-onready var player = global.get("player")
-onready var object = get_node("Sprite")
-var enemy_speed = 100
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_physics_process(false)
+	_velocity.x = -speed.x
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var dir = (object.global_position - global_position)
+	_velocity.y += gravity * delta
+	if is_on_wall():
+		_velocity.x *= -1.0
+		
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
