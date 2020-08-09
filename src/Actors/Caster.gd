c533ff699d111561b89275993442aa3b88aa7750
Conner Hinkle <connerhinkle@gmail.com>
2020-08-09 00:55:08 -0700

    Fixed ERROR("Invalid get index 'global_transform' on base 'previously freed instance") LINE 9. Now check if player is a valid instance in both Caster and the projectile.

--- src/Actors/Caster.gd
+++ src/Actors/Caster.gd
@@ -6,13 +6,14 @@ var timer = Timer.new()
 const bullet = preload("res://src/Actors/CasterProjectile.tscn")
 
 func _process(delta):
-	if global.get("player").global_position.x - self.global_position.x > 0:
-		get_node("Sprite").flip_h = true
-	else:
-		get_node("Sprite").flip_h = false
-	if abs(self.global_transform.origin.x - player.global_transform.origin.x) > 400 and can_fire:
-		timer.start()
-		can_fire = false
+	if is_instance_valid( player ):
+		if global.get("player").global_position.x - self.global_position.x > 0:
+			get_node("Sprite").flip_h = true
+		else:
+			get_node("Sprite").flip_h = false
+		if abs(self.global_transform.origin.x - player.global_transform.origin.x) > 400 and can_fire:
+			timer.start()
+			can_fire = false
 	
 func fire():
 	can_fire = true
--- src/Actors/CasterProjectile.gd
+++ src/Actors/CasterProjectile.gd
@@ -5,6 +5,7 @@ extends RigidBody2D
 export var projectile_speed = 200
 export var lifetime = 2
 var velocity = Vector2()
+var player = global.get("player")
 
 func _physics_process(delta: float) -> void:
 	#ACTIVATE LINE BELOW TO MAKE PROJECTILE FOLLOW PLAYER
@@ -13,19 +14,21 @@ func _physics_process(delta: float) -> void:
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity = (self.global_position - global.get("player").global_position).normalized()
-	set_physics_process(true)
-	print("ENEMY SHOT")
+	if is_instance_valid( player ):
+		velocity = (self.global_position - player.global_position).normalized()
+		set_physics_process(true)
+		print("ENEMY SHOT")
 	
 func spawn(position):
-	if global.get("player").global_position.x - position.x < 0:
-		self.global_position = position - Vector2(100, 0)
-	else:
-		self.global_position = position - Vector2(-100, 0)
-	rotation = global.get("player").global_transform.origin.angle_to(self.global_transform.origin)
-	velocity = global_position.direction_to(global.get("player").global_position)
-	yield(get_tree().create_timer(lifetime), "timeout")
-	queue_free()
+	if is_instance_valid( player ):
+		if global.get("player").global_position.x - position.x < 0:
+			self.global_position = position - Vector2(100, 0)
+		else:
+			self.global_position = position - Vector2(-100, 0)
+		rotation = global.get("player").global_transform.origin.angle_to(self.global_transform.origin)
+		velocity = global_position.direction_to(global.get("player").global_position)
+		yield(get_tree().create_timer(lifetime), "timeout")
+		queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
